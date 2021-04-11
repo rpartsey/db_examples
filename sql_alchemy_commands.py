3185bb669c4b6aa5853c1d2809ddc431a960b65b
rpartsey <partsey2412@gmail.com>
2021-04-11 16:04:12 +0300

    added sqlalchemy demo

---
+++ sql_alchemy_commands.py
@@ -0,0 +1,121 @@
+from datetime import date
+
+from sql_alchemy_demo.actor import Actor
+from sql_alchemy_demo.base import Session, engine, Base
+from sql_alchemy_demo.contact_details import ContactDetails
+from sql_alchemy_demo.movie import Movie
+from sql_alchemy_demo.stuntman import Stuntman
+
+# 2 - generate database schema
+Base.metadata.create_all(engine)
+
+# --------------------------------------------------
+#   inserts
+#--------------------------------------------------
+
+# 3 - create a new session
+session = Session()
+
+# 4 - create movies
+bourne_identity = Movie("The Bourne Identity", date(2002, 10, 11))
+furious_7 = Movie("Furious 7", date(2015, 4, 2))
+pain_and_gain = Movie("Pain & Gain", date(2013, 8, 23))
+
+# 5 - creates actors
+matt_damon = Actor("Matt Damon", date(1970, 10, 8))
+dwayne_johnson = Actor("Dwayne Johnson", date(1972, 5, 2))
+mark_wahlberg = Actor("Mark Wahlberg", date(1971, 6, 5))
+
+# 6 - add actors to movies
+bourne_identity.actors = [matt_damon]
+furious_7.actors = [dwayne_johnson]
+pain_and_gain.actors = [dwayne_johnson, mark_wahlberg]
+
+# 7 - add contact details to actors
+matt_contact = ContactDetails("415 555 2671", "Burbank, CA", matt_damon)
+dwayne_contact = ContactDetails("423 555 5623", "Glendale, CA", dwayne_johnson)
+dwayne_contact_2 = ContactDetails("421 444 2323", "West Hollywood, CA", dwayne_johnson)
+mark_contact = ContactDetails("421 333 9428", "Glendale, CA", mark_wahlberg)
+
+# 8 - create stuntmen
+matt_stuntman = Stuntman("John Doe", True, matt_damon)
+dwayne_stuntman = Stuntman("John Roe", True, dwayne_johnson)
+mark_stuntman = Stuntman("Richard Roe", True, mark_wahlberg)
+
+# 9 - persists data
+session.add(bourne_identity)
+session.add(furious_7)
+session.add(pain_and_gain)
+
+session.add(matt_contact)
+session.add(dwayne_contact)
+session.add(dwayne_contact_2)
+session.add(mark_contact)
+
+session.add(matt_stuntman)
+session.add(dwayne_stuntman)
+session.add(mark_stuntman)
+
+# 10 - commit and close session
+session.commit()
+session.close()
+
+
+# --------------------------------------------------
+#   queries
+#--------------------------------------------------
+# 2 - extract a session
+session = Session()
+
+# 3 - extract all movies
+movies = session.query(Movie).all()
+
+# 4 - print movies' details
+print('\n### All movies:')
+for movie in movies:
+    print(f'{movie.title} was released on {movie.release_date}')
+print('')
+
+# The most important functions:
+# - count(): Returns the total number of rows of a query.
+# - filter(): Filters the query by applying a criteria.
+# - delete(): Removes from the database the rows matched by a query.
+# - distinct(): Applies a distinct statement to a query.
+# - exists(): Adds an exists operator to a subquery.
+# - first(): Returns the first row in a query.
+# - get(): Returns the row referenced by the primary key parameter passed as argument.
+# - join(): Creates a SQL join in a query.
+# - limit(): Limits the number of rows returned by a query.
+# - order_by(): Sets an order in the rows returned by a query.
+
+# 5 - get movies after 15-01-01
+movies = session.query(Movie) \
+    .filter(Movie.release_date > date(2015, 1, 1)) \
+    .all()
+
+print('### Recent movies:')
+for movie in movies:
+    print(f'{movie.title} was released after 2015')
+print('')
+
+# 6 - movies that Dwayne Johnson participated
+the_rock_movies = session.query(Movie) \
+    .join(Actor, Movie.actors) \
+    .filter(Actor.name == 'Dwayne Johnson') \
+    .all()
+
+print('### Dwayne Johnson movies:')
+for movie in the_rock_movies:
+    print(f'The Rock starred in {movie.title}')
+print('')
+
+# 7 - get actors that have house in Glendale
+glendale_stars = session.query(Actor) \
+    .join(ContactDetails) \
+    .filter(ContactDetails.address.ilike('%glendale%')) \
+    .all()
+
+print('### Actors that live in Glendale:')
+for actor in glendale_stars:
+    print(f'{actor.name} has a house in Glendale')
+print('')
